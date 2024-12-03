a79b8415f1c0606b11cdb57d8c641911fc226eac
Peter Santesson <santessonpeter97@gmail.com>
2024-12-03 09:53:26 +0100

    add boilerplate

---
+++ AoC24/src/boilerplate/boilerplate.ts
@@ -0,0 +1,39 @@
+import { readLines } from "../util"
+
+// First Solution
+const main1 = async () => {
+  const groups = await readLines();
+  
+  const inputs1 = groups[0];
+  const inputs2 = groups[1];
+  const inputs3 = groups[2];
+
+  let result = 0;
+
+  for (let i = 0; i < inputs1.length; i++) {
+    const element = inputs1[i];
+    
+  }
+
+  console.log(result);
+}
+
+// Second Solution
+const main2 = async () => {
+  const groups = await readLines();
+  
+  const inputs1 = groups[0];
+  const inputs2 = groups[1];
+  const inputs3 = groups[2];
+
+  let result = 0;
+
+  for (let i = 0; i < inputs1.length; i++) {
+    const element = inputs1[i];
+    
+  }
+
+  console.log(result);
+}
+
+main1();
