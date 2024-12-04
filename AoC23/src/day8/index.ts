353063cdcb07986aa4ef8bb269c8103d07fc451c
Peter Santesson <santessonpeter97@gmail.com>
2024-12-04 14:44:37 +0100

    year 23 day 8

--- AoC23/src/day8/index.ts
+++ AoC23/src/day8/index.ts
@@ -1,5 +1,5 @@
 
-export const solution = (inputs: string[][]) => {
+export const solution1 = (inputs: string[][]) => {
   const map = new Map<string, string[]>();
 
   const instructions = inputs[0][0];
@@ -30,4 +30,73 @@ export const solution = (inputs: string[][]) => {
   }
 
   console.log(iterations * instructions.length);
+}
+
+export const solution2 = (inputs: string[][]) => {
+  const map = new Map<string, string[]>();
+
+  const instructions = inputs[0][0];
+
+  const startPos: string[] = [];
+  for (let i = 0; i < inputs[1].length; i++) {
+    const str = inputs[1][i];
+    
+    const [key, value] = str.split(' = ');
+    map.set(key, value.replace('(', '').replace(')', '').split(', '));
+    if (key.endsWith('A')) startPos.push(key);
+  }
+
+  const hasEndPosition = (pos: string[]): boolean => {
+    for (let i = 0; i < pos.length; i++) {
+      if (pos[i].endsWith('Z')) return true;
+    }
+    return false;
+  }
+
+  let iterations = 0;
+
+  const followInstructions = (ins: string, i: number, pos: string[]) => {
+    if (i === ins.length) return pos;
+
+    const nextPos: string[] = [];
+    for (let j = 0; j < pos.length; j++) {
+      const currOptions = map.get(pos[j]) || [];
+      if (ins.charAt(i) === 'L') {
+        nextPos.push(currOptions[0]);
+      } else {
+        nextPos.push(currOptions[1]);
+      }
+    }
+
+    return followInstructions(ins, i+1, nextPos);
+  }
+
+  const iterationsToEndPos = []
+  let positions = [...startPos];
+  while (iterationsToEndPos.length < startPos.length) {
+    iterations++;
+    positions = followInstructions(instructions, 0, positions);
+
+    if (hasEndPosition(positions)) {
+      iterationsToEndPos.push(iterations);
+    }
+  }
+
+  iterationsToEndPos.sort((a, b) => b - a)
+
+  let index = 0;
+  let prediction = 0;
+  outer: while (true) {
+    index++;
+    prediction = iterationsToEndPos[0] * index;
+
+    for (let i = 1; i < iterationsToEndPos.length; i++) {
+      if (prediction % iterationsToEndPos[i] !== 0) {
+        break;
+      }
+      if (i === iterationsToEndPos.length-1) break outer;
+    }
+  }
+
+  console.log(prediction * instructions.length);
 }
